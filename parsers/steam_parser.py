3e19e711beed48db1636f0aef1edf929bebbd559
Bunyod <b.eshkuvvatov@gmail.com>
2026-04-02 13:07:04 +0300

    тестовая версия

--- parsers/steam_parser.py
+++ parsers/steam_parser.py
@@ -1,6 +1,20 @@
-from base_parser import Base_Parser
-from game_price import Game_Price
+#from base_parser import Base_Parser
+#from game_price import Game_Price
+import requests as req
+from bs4 import BeautifulSoup
 
-class Steam_Parser(Base_Parser):
+class Steam_Parser:
     def game_search(self, game_name):
         pass
+    
+    def get_appid(self, game_name):
+        url = f"https://store.steampowered.com/search/?term={game_name}" # Это url для поиска игр по имени, он возвращает страницу с результатами поиска, где можно найти appid игры
+        response = req.get(url) # Получаем HTML-код страницы
+        soup = BeautifulSoup(response.text, 'html.parser') # Парсим HTML-код с помощью BeautifulSoup
+        search_results = soup.select("a.search_result_row")
+        for result in search_results: # Проходим по всем результатам поиска
+            title = result.select_one('span', class_='title').text.strip() # Получаем название игры
+            if title.lower() == game_name.lower(): # Если название игры совпадает с искомым, то возвращаем appid
+                appid = result['data-ds-appid'] # Получаем appid из атрибута data-ds-appid
+                return appid
+            
