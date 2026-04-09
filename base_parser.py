d7ad3d47170ed12ed347462fcf323754f4f6ae6a
aangelicm <makves193@icloud.com>
2026-04-09 15:30:15 +0300

    ab

--- base_parser.py
+++ base_parser.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 from abc import ABC, abstractmethod
-=======
-from abs import ABC, abstractmethod
->>>>>>> 438888eb6198002ea7fda2c3e54e4e0e02c3f483
 
 class Base_Parser(ABC):
     @abstractmethod
-    def game_search(self, game_name):
-        """Search game by its name from site and return GamePrice object"""
+    def game_search(self, game_name, site_name=None, price=None, currency=None, type=None, url=None):
         pass
-    
